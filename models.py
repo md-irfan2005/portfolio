83d4fe7aec25bde3be09461121278928c59d8323
ifanhak <irfanimd481@gmail.com>
2025-07-05 17:35:49 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,12 @@
+# models.py
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    message = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
